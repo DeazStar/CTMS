981a20562eef57124ba6f49c92169cd92ec9ca58
DeazStar <deadfromtheborn@gmail.com>
2024-02-19 11:59:21 +0300

    add the ability to send an email

---
+++ server/utils/Email.js
@@ -0,0 +1,62 @@
+import nodemailer from 'nodemailer';
+import { convert } from 'html-to-text';
+import pug from 'pug';
+
+export class Email {
+  constructor(obj) {
+    this.from = process.env.EMAIL;
+    this.to = obj.to;
+
+    if (obj.url) {
+      this.url = obj.url;
+    }
+  }
+
+  createTransporter() {
+    if (process.env.NODE_EVN === 'development') {
+      return nodemailer.createTransport({
+        host: process.env.DEV_HOST,
+        port: process.env.DEV_PORT,
+        secure: false,
+        auth: {
+          user: process.env.DEV_USER,
+          pass: process.env.DEV_PASS,
+        },
+      });
+    } else {
+      return nodemailer.createTransport({
+        host: process.env.PROD_HOST,
+        port: process.env.PROD_PORT,
+        secure: true,
+        auth: {
+          user: process.env.PROD_USER,
+          pass: process.env.PROD_PASS,
+        },
+      });
+    }
+  }
+
+  async sendInvetation(inviter, project) {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/invetationEmail.pug`,
+      {
+        subject: 'Invetation to CTMS project',
+        to: this.to,
+        inviter: inviter,
+        project: project,
+      },
+    );
+
+    const text = convert(html);
+
+    const mailOption = {
+      from: this.from,
+      to: this.to,
+      subject: 'Invetation to CTMS project',
+      text: text,
+      html: html,
+    };
+
+    await this.createTransporter().sendMail(mailOption);
+  }
+}
