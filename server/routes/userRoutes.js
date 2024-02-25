bf203c53cf168ab82a10e0ed76003528f122a86f
DeazStar <deadfromtheborn@gmail.com>
2024-02-25 14:12:08 +0300

    allow user to update password, profile like name and update profile image

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import { signup, login, protectRoute } from '../controllers/authController.js';
+import {
+  updateProfile,
+  updatePassword,
+  updateAvatar,
+} from '../controllers/userController.js';
+import multer from 'multer';
+
+const upload = multer({ dest: `${process.cwd()}/public/user/` });
 
 const router = express.Router();
 
@@ -8,4 +16,9 @@ router.route('/login').post(login);
 
 router.use(protectRoute);
 
+router.route('/profile/update-profile').post(updateProfile);
+router.route('/profile/update-password').post(updatePassword);
+router
+  .route('/profile/update-avatar')
+  .post(upload.single('avatar'), updateAvatar);
 export default router;
